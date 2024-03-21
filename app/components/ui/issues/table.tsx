5bb3d66f697b6be28d8cbb2e321349edec61e474
yingtu35 <daniel860305@gmail.com>
2024-03-21 14:19:08 +0800

    add toast notification for signOut and closeIssue

--- app/components/ui/issues/table.tsx
+++ app/components/ui/issues/table.tsx
@@ -16,31 +16,7 @@ export default function FilteredIssuesTable({
   nextPageUrl: string | null
 }) {
   const { ref, inView, entry } = useInView();
-  // const [issues, setIssues] = useState(initialIssues);
-  // const [nextPage, setNextPage] = useState(nextPageUrl);
   const { issues, nextPage } = useIssues(inView, initialIssues, nextPageUrl);
-
-  // useEffect(() => {
-  //   console.log("nextPageUrl:", nextPageUrl);
-  //   setIssues(initialIssues);
-  //   setNextPage(nextPageUrl); // Provide a default value of an empty string if nextPageUrl is null
-  // }, [initialIssues, nextPageUrl]);
-
-  // // add a useEffect to fetch more issues when inView is true
-  // useEffect(() => {
-  //   async function fetchMoreIssues() {
-  //     if (inView) {
-  //       console.log("fetch more issues");
-  //       // await new Promise((resolve) => setTimeout(resolve, 1000));
-  //       if (nextPage) {
-  //         const [moreIssues, nextUrl] = await getMoreIssues(nextPage);
-  //         setIssues([...issues, ...moreIssues]);
-  //         setNextPage(nextUrl);
-  //       }
-  //     }
-  //   }
-  //   fetchMoreIssues();
-  // }, [inView]);
 
   return (
       <div className="flex flex-col gap-4">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 import Header from "@/app/components/ui/header";
 import Footer from "@/app/components/ui/footer";
 import BackToTop from "@/app/components/ui/backToTop";
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster richColors closeButton position="top-center" offset={10} />
         <Header />
         <main className="flex-auto w-full max-w-3xl px-4 py-4 mx-auto sm:px-6 md:py-6">
           {children}
